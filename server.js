30703b576a2ccab883c4f4e42bfdfe1c15c35462
Saurabhvishv <saurabh2vishv@gmail.com>
2023-01-04 10:52:10 +0530

    view_Images

--- server.js
+++ server.js
@@ -4,62 +4,89 @@ const path = require('path');
 const cors = require('cors');
 const multer = require('multer');
 const port = process.env.PORT || 5000;
+let Client = require('ssh2-sftp-client');
 
+app.use('/public/uploads', express.static('./public/uploads'));
 app.use(cors());
+
 var counts = 1;
+
 const storage = multer.diskStorage({
     destination: './public/uploads/',
-    filename: function (req, file, cb) {   
-        
-        cb(null, `${counts++}${path.extname(file.originalname)}`)  
+    filename: function (req, file, cb) {
+
+        cb(null, `${counts++}${path.extname(file.originalname)}`)
     }
 })
 
-
-const upload = multer({ storage: storage,
-    fileFilter: function(req, file, cb){
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
         checkFileType(file, cb);
     }
-     }).single("myFile");
-    
-    function checkFileType(file, cb){
-        const filetypes= /jpeg|jpg|png|gif|webp/;
-        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-        const mimetype = filetypes.test(file.mimetype);
-        if(mimetype &&  extname){
-            return cb(null, true);
-        }else{
-            cb('ERROR: Images Only!');
-        }
-    }
+}).single("myFile");
 
-    app.use('/public/uploads',express.static('./public/uploads'));
-    app.get('/',(req, res)=> res.send('this is working'));
-app.post('/imageupload', async (req, res) => {	
+app.get('/', (req, res) => {
+    let sftp = new Client();
+    var temp = [];
+    sftp.connect({
+        host: '139.59.65.197',
+        port: '22',
+        username: 'root',
+        password: '880088@#Blockchain'
+    }).then(() => {
+        return sftp.list('/var/lib/docker/overlay2/b654a7ff6fbba58b14c714a3fafc13a0e037002b461d2fcebd568cbfca134b21/diff/app/public/uploads');
+    }).then(data => {
+
+        data.forEach(element => {
+            let imagePath = "http://139.59.65.197:5000/public/uploads/" + element.name;
+            temp.push(imagePath)
+            if (temp.length == data.length) {
+                res.send(temp)
+            }
+
+        });
+    }).catch(err => {
+        console.log(err, 'catch error');
+    });
+});
+
+app.post('/imageupload', async (req, res) => {
     try {
-        upload(req, res, function(err) {
-            
+        upload(req, res, function (err) {
 
-            if(err){
-                res.send({msg:err});
-            }else{
-                if(req.file == undefined){
+
+            if (err) {
+                res.send({ msg: err });
+            } else {
+                if (req.file == undefined) {
                     res.send({
                         msg: "select image"
                     });
-                }else{
+                } else {
                     res.json({
-                        msg:'File Uploaded',
+                        msg: 'File Uploaded',
                         file: `${req.file.filename}`
                     })
                     // console.log(req.file.filename);
-                    
+
                 }
             }
 
-        }); 
+        });
 
-    }catch (err) {console.log(err)}
+    } catch (err) { console.log(err) }
 })
+
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png|gif|webp/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('ERROR: Images Only!');
+    }
+}
 
 app.listen(port, () => console.log(`${port}`))
