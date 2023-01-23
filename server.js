511d31ef6aacd176709932a3fc811039671d8d1c
Saurabhvishv <saurabh2vishv@gmail.com>
2023-01-23 15:44:44 +0530

    serve

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(fileUpload());
 app.use('/images', express.static('./images'));
 
-
+//
 app.post('/upload', function (req, res) {
     let sampleFile;
     let uploadPath;
