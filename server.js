75485eb14754f758e226b5b8ff9c1c1027ae7702
Saurabhvishv <saurabh2vishv@gmail.com>
2023-02-08 13:02:39 +0530

    old

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const port = process.env.PORT || 5000;
 const express = require('express');
-const multer = require('multer');
 const app = express();
 const cors = require('cors');
 var bodyParser = require('body-parser')
@@ -36,19 +35,6 @@ app.post('/upload', function (req, res) {
     });
 });
 
-// app.post("/upload", upload.array("file"), (req, res) => {
-
-
-//     console.log(req.body);
-//     console.log(req.files);
-//     res.json({ message: "File(s) uploaded successfully" });
-
-// });
-
-app.listen(5000, function(){
-    console.log("Server running on port 5000");
-});
-
 app.get("/", (req, res) => {
     fs.readdir(dirPath, (err, images) => {
         return res.send(images);
@@ -75,41 +61,3 @@ app.delete("/", (req, res) => {
 });
 
 app.listen(port, () => console.log(`${port}`))
-
-
-
-// const express = require("express");
-// const multer = require('multer');
-// const cors = require('cors');
-
-// var app = express();
-// app.use(cors());
-
-
-// const storage = multer.diskStorage({
-//     // uploadPath = __dirname + '/images/' + sampleFile.name;
-//     destination: function (req, file, callback) {
-//         callback(null, __dirname + '/images');
-//     },
-//     // Sets file(s) to be saved in uploads folder in same directory
-//     filename: function (req, file, callback) {
-//         callback(null, file.originalname);
-//     }
- 
-//   })
-  
-
-// const upload = multer({ storage: storage })
-
-// app.post("/upload", upload.array("file"), (req, res) => {
-
-
-//     console.log(req.body);
-//     console.log(req.files);
-//     res.json({ message: "File(s) uploaded successfully" });
-
-// });
-
-// app.listen(5000, function(){
-//     console.log("Server running on port 5000");
-// });
